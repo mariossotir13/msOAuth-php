d8050585457994d7e1ccd5af1923bc1d5caca8a3
Marios Sotiropoulos <mai1211@uom.edu.gr>
2013-11-02 06:31:08 -0700

    Generate setters and getters for the client entity.
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

--- src/Ms/OauthBundle/Entity/Client.php
+++ src/Ms/OauthBundle/Entity/Client.php
@@ -11,25 +11,92 @@ use Ms\OauthBundle\Entity\User;
  * @author user
  */
 class Client extends User {
+
     /**
      * Ο τίτλος της εφαρμογής.
      * 
-     * @ var string.
+     * @var string
      */
     protected $appTitle;
+
     /**
      * To URI στο οποίο ανακατευθύνεται ο ιδιοκτήτης του πόρου μετά από μια 
      * επιτυχημένη εγγραφή.
      * 
      * Παρέχεται από τον πελάτη κατά την εγγραφή του.
      * 
-     * @ var string.
+     * @var string
      */
     protected $redirectionUri;
+
     /**
      * Ο τύπος του Πελάτη.
      * 
-     * @ var int.
+     * @var int
      */
     protected $clientType;
+
+    /**
+     * Set appTitle
+     *
+     * @param string $appTitle
+     * @return Client
+     */
+    public function setAppTitle($appTitle) {
+        $this->appTitle = $appTitle;
+
+        return $this;
+    }
+
+    /**
+     * Get appTitle
+     *
+     * @return string 
+     */
+    public function getAppTitle() {
+        return $this->appTitle;
+    }
+
+    /**
+     * Set redirectionUri
+     *
+     * @param string $redirectionUri
+     * @return Client
+     */
+    public function setRedirectionUri($redirectionUri) {
+        $this->redirectionUri = $redirectionUri;
+
+        return $this;
+    }
+    
+    /**
+     * Get redirectionUri
+     *
+     * @return string 
+     */
+    public function getRedirectionUri() {
+        return $this->redirectionUri;
+    }
+
+    /**
+     * Set clientType
+     *
+     * @param integer $clientType
+     * @return Client
+     */
+    public function setClientType($clientType) {
+        $this->clientType = $clientType;
+
+        return $this;
+    }
+
+    /**
+     * Get clientType
+     *
+     * @return integer 
+     */
+    public function getClientType() {
+        return $this->clientType;
+    }
+
 }
