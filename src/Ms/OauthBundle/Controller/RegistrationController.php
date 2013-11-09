227f5c06392e18680a33de8b14672034613d89a4
Marios Sotiropoulos <mai1211@uom.edu.gr>
2013-11-09 15:17:46 -0800

    Add Form/Type/UserType.php.
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

--- src/Ms/OauthBundle/Controller/RegistrationController.php
+++ src/Ms/OauthBundle/Controller/RegistrationController.php
@@ -3,12 +3,37 @@
 namespace Ms\OauthBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Ms\OauthBundle\Entity\User;
+use Ms\OauthBundle\Form\Type\UserType;
+use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * 
+ */
 class RegistrationController extends Controller {
 
-    public function clientAction() {
+    /**
+     * 
+     * @return type
+     */
+    public function clientAction(Request $request) {
+        $user = new User();
+        // TODO: Generate user ID and password.
+        $user->setId('1')
+            ->setPassword('1');
+        $form = $this->createForm(new UserType(), $user);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $em->flush();
+        }
+        
         return $this->render(
-            'MsOauthBundle:Registration:client.html.twig'
+            'MsOauthBundle:Registration:client.html.twig', 
+            array('form' => $form->createView())
         );
     }
+
 }
---
+++ src/Ms/OauthBundle/Form/Type/UserType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Ms\OauthBundle\Form\Type;
+
+use \Symfony\Component\Form\AbstractType;
+use \Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use \Symfony\Component\Form\FormBuilderInterface;
+/**
+ * Description of UserType
+ *
+ * @author user
+ */
+class UserType extends AbstractType {
+    
+    /**
+     * @inheritdoc
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('email', 'email')
+            ->add('Submit', 'submit');
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getName() {
+        return 'ms_oauthbundle_user';
+    }
+    
+    /**
+     * @inheritdoc
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver) {
+        parent::setDefaultOptions($resolver);
+        $resolver->setDefaults(array(
+            'data_class' => 'Ms\OauthBundle\Entity\User'
+        ));
+    }
+}
