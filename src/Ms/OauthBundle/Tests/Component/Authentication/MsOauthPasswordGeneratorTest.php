85ff40bebca14876bcec3ca4cb31551b6240f96b
Marios Sotiropoulos <mai1211@uom.edu.gr>
2013-11-22 18:24:03 +0200

    Use the "salts" created during the "testCreateSalt" tests in the "testCreatePassword" tests.
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

--- src/Ms/OauthBundle/Tests/Component/Authentication/MsOauthPasswordGeneratorTest.php
+++ src/Ms/OauthBundle/Tests/Component/Authentication/MsOauthPasswordGeneratorTest.php
@@ -27,10 +27,32 @@ class MsOauthPasswordGeneratorTest extends \PHPUnit_Framework_TestCase {
     private $generator;
     
     /**
+     * Εξετάζει τη μοναδικότητα των αποτελεσμάτων της μεθόδου *createSalt*.
+     * 
+     * Για μία λεπτομερή περιγραφή του αλγόριθμου της δοκιμής δείτε τη δοκιμή
+     * *testCreatePassword*.
+     */
+    public function testCreateSalt() {
+        $results = array();
+        for ($i = 0; $i < static::$RESULTS_TOTAL; $i++) {
+            $salt = $this->generator->createSalt();
+            if (in_array($salt, $results)) {
+                $this->fail("Duplicate salt: {$salt} (iteration: {$i}");
+            }
+            $results[] = $salt;
+        }
+        
+        return $results;
+    }
+    
+    /**
      * Εξετάζει τη μοναδικότητα των αποτελεσμάτων της μεθόδου *createPassword*.
      * 
-     * Καθώς αναμένεται κάθε τιμή να είναι τυχαία δεν μπορούν να ελεγχθούν με ένα
-     * προϋπολογισμένο σύνολο αποτελεσμάτων.
+     * Οι δοκιμές γίνονται χρησιμοποιώντας τα "αλάτια" τα οποία παράγονται κατά
+     * τις δοκιμές της μεθόδου *testCreatePasswordSalt*.
+     * 
+     * Καθώς αναμένεται κάθε τιμή να είναι τυχαία οι κωδικοί δεν μπορούν να
+     * ελεγχθούν με ένα προϋπολογισμένο σύνολο αποτελεσμάτων.
      * 
      * Η λύση την οποία ακολουθούμε είναι να παράγουμε ένα αρκετά μεγάλο πλήθος
      * αποτελεσμάτων και να τα αποθηκεύουμε καθώς παράγονται. Επιπλέον, μετά την
@@ -42,33 +64,23 @@ class MsOauthPasswordGeneratorTest extends \PHPUnit_Framework_TestCase {
      * Με τον όρο "αρκετά μεγάλο πλήθος" εννοούμε μία τιμή η οποία μας επιτρέπει
      * να εκτελούμε τις δοκιμές σε ένα εύλογο χρονικό διάστημα. Για παράδειγμα,
      * σε λιγότερο από δέκα δευτερόλεπτα.
+     * 
+     * @depends testCreatePasswordSalt
+     * 
+     * TODO: Ίσως πρέπει να αφαιρεθεί η εξάρτηση από τη μέθοδο testCreatePasswordSalt
+     * και να βρούμε άλλη λύση. Για παράδειγμα, θα μπορούσαμε να χρησιμοποιούμε
+     * το ίδιο "αλάτι" για όλους τους παραγόμενους κωδικούς στηριζόμενοι στο γεγονός
+     * ότι εάν δεν αποτυγχάνει με το ίδιο "αλάτι" δεν υπάρχει περίπτωση να αποτύχει
+     * με τυχαίο.
      */
-    public function testCreatePassword() {
-        $salt = '1';
+    public function testCreatePassword(array $salts) {
         $results = array();
         for ($i = 0; $i < static::$RESULTS_TOTAL; $i++) {
-            $password = $this->generator->createPassword($salt);
+            $password = $this->generator->createPassword($salts[$i]);
             if (in_array($password, $results)) {
                 $this->fail("Duplicate password: {$password} (iteration: {$i}");
             }
             $results[] = $password;
-        }
-    }
-    
-    /**
-     * Εξετάζει τη μοναδικότητα των αποτελεσμάτων της μεθόδου *createSalt*.
-     * 
-     * Για μία λεπτομερή περιγραφή του αλγόριθμου της δοκιμής δείτε τη δοκιμή
-     * *testCreatePassword*.
-     */
-    public function testCreatePasswordSalt() {
-        $results = array();
-        for ($i = 0; $i < static::$RESULTS_TOTAL; $i++) {
-            $salt = $this->generator->createSalt();
-            if (in_array($salt, $results)) {
-                $this->fail("Duplicate salt: {$salt} (iteration: {$i}");
-            }
-            $results[] = $salt;
         }
     }
     
