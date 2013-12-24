cbafafc892e68170ba3e75f7db8fbd98466a71ea
Marios Sotiropoulos <mai1211@uom.edu.gr>
2013-12-24 12:42:39 +0200

    Relate Client to AuthorizationCodeProfile entities.
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

--- src/Ms/OauthBundle/Entity/Client.php
+++ src/Ms/OauthBundle/Entity/Client.php
@@ -3,6 +3,8 @@
 namespace Ms\OauthBundle\Entity;
 
 use Ms\OauthBundle\Entity\User;
+use Ms\OauthBundle\Entity\AuthorizationCodeProfile;
+use Doctrine\Common\Collections\ArrayCollection;
 
 /**
  * Μία εφαρμογή η οποία κάνει αιτήσεις για προστατευμένο πόρο εκ μέρους του
@@ -20,6 +22,14 @@ class Client extends User {
     protected $appTitle;
 
     /**
+     * Τα προφίλ των Κωδικών Εξουσιοδότησης τους οποίους έχει εκδώσει το σύστημα
+     * για τον παρόντα Πελάτη.
+     *
+     * @var AuthorizationCodeProfile[]
+     */
+    protected $authorizationCodeProfiles;
+
+    /**
      * To URI στο οποίο ανακατευθύνεται ο ιδιοκτήτης του πόρου μετά από μια 
      * επιτυχημένη εγγραφή.
      * 
@@ -35,6 +45,25 @@ class Client extends User {
      * @var int
      */
     protected $clientType;
+
+    /**
+     * Δημιουργεί ένα νέο στιγμιότυπο της κλάσης Client.
+     */
+    function __construct() {
+        $this->authorizationCodeProfiles = new ArrayCollection();
+    }
+
+    /**
+     * Add authorizationCodeProfiles
+     *
+     * @param AuthorizationCodeProfile $authorizationCodeProfile
+     * @return Client
+     */
+    public function addAuthorizationCodeProfile(AuthorizationCodeProfile $authorizationCodeProfile) {
+        $this->authorizationCodeProfiles[] = $authorizationCodeProfile;
+
+        return $this;
+    }
 
     /**
      * Set appTitle
@@ -68,7 +97,7 @@ class Client extends User {
 
         return $this;
     }
-    
+
     /**
      * Get redirectionUri
      *
@@ -97,6 +126,24 @@ class Client extends User {
      */
     public function getClientType() {
         return $this->clientType;
+    }
+
+    /**
+     * Remove authorizationCodeProfiles
+     *
+     * @param AuthorizationCodeProfile $authorizationCodeProfile
+     */
+    public function removeAuthorizationCodeProfile(AuthorizationCodeProfile $authorizationCodeProfile) {
+        $this->authorizationCodeProfiles->removeElement($authorizationCodeProfile);
+    }
+
+    /**
+     * Get authorizationCodeProfiles
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getAuthorizationCodeProfiles() {
+        return $this->authorizationCodeProfiles;
     }
 
 }
