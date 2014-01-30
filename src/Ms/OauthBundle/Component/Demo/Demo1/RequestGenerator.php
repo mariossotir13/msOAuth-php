bea077741588457f16c6a116216c59a9610dc563
Marios Sotiropoulos <mai1211@uom.edu.gr>
2014-01-30 11:05:29 +0200

    Add demo scenarios for the authorization code in "Demo 1".
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

---
+++ src/Ms/OauthBundle/Component/Demo/Demo1/RequestGenerator.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace Ms\OauthBundle\Component\Demo\Demo1;
+
+use Ms\OauthBundle\Component\Authorization\AccessTokenRequest;
+use Ms\OauthBundle\Component\Authorization\AuthorizationRequest;
+use Ms\OauthBundle\Entity\AuthorizationCodeScope;
+use Ms\OauthBundle\Component\Authorization\AuthorizationResponseType;
+
+/**
+ * Description of RequestGenerator
+ *
+ * @author Marios
+ */
+class RequestGenerator {
+    
+    /**@#+
+     *
+     * @var string
+     */
+    private static $AUTHORIZATION_RESPONSE_TYPE_UNSUPPORTED = 'c';
+    private static $CLIENT_ID = 'zMuobKhbnvJUTYc+EnXfRwiiHP4/OpmM5CLrdpkIsm4';
+    private static $CLIENT_ID_INVALID = 'z';
+    private static $REDIRECTION_URI = 'http://msoauthphp.local/app_dev.php/client-app/demo1';
+    private static $REDIRECTION_URI_INVALID = 'http://msoauthphp.local/app_dev.php/client-app/demo1#a';
+    private static $PASSWORD = 'fmodKwVrRQOC2Io7TpWu0VDkTrA=';
+    private static $SCOPE_INVALID = 'c';
+    private static $STATE = 'RdoTKJnaUxdRfE7QBTZX';
+    /**@#-*/
+    
+    /**
+     * 
+     * @param string $code
+     * @param boolean $asUri
+     * @return AccessTokenRequest
+     */
+    public function createAccessTokenRequest($code, $asUri = false) {
+        $request = new AccessTokenRequest(AccessTokenRequest::SERVER_URI);
+        $request->setClientId(static::$CLIENT_ID);
+        $request->setCode($code);
+        $request->setGrantType('authorization_code');
+        $request->setRedirectionUri(static::$REDIRECTION_URI);
+        
+        return $asUri ? $request->toUri(true) : $request;
+    }
+    
+    /**
+     * 
+     * @param string $code
+     * @param boolean $asUri
+     * @return AccessTokenRequest
+     */
+    public function createAccessTokenRequestWithMissingRequiredParameter($code, $asUri = false) {
+        $request = new AccessTokenRequest(AccessTokenRequest::SERVER_URI);
+        $request->setClientId(static::$CLIENT_ID);
+        $request->setCode($code);
+        $request->setRedirectionUri(static::$REDIRECTION_URI);
+        
+        return $asUri ? $request->toUri(true) : $request;
+    }
+    
+    /**
+     * 
+     * @param boolean $asUri
+     * @return string | AuthorizationRequest
+     */
+    public function createAuthorizationRequest($asUri = false) {
+        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+        $request->addScope(AuthorizationCodeScope::BASIC);
+        $request->setClientId(static::$CLIENT_ID);
+        $request->setRedirectionUri(static::$REDIRECTION_URI);
+        $request->setResponseType(AuthorizationResponseType::CODE);
+        $request->setState(static::$STATE);
+        
+        return $asUri ? $request->toUri() : $request;
+    }
+    
+    /**
+     * 
+     * @param boolean $asUri
+     * @return string | AuthorizationRequest
+     */
+    public function createAuthorizationRequestWithInvalidClientId($asUri = false) {
+        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+        $request->addScope(AuthorizationCodeScope::BASIC);
+        $request->setClientId(static::$CLIENT_ID_INVALID);
+        $request->setRedirectionUri(static::$REDIRECTION_URI);
+        $request->setResponseType(AuthorizationResponseType::CODE);
+        $request->setState(static::$STATE);
+        
+        return $asUri ? $request->toUri() : $request;
+    }
+    
+    /**
+     * 
+     * @param boolean $asUri
+     * @return string | AuthorizationRequest
+     */
+    public function createAuthorizationRequestWithInvalidRedirectionUri($asUri = false) {
+        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+        $request->addScope(AuthorizationCodeScope::BASIC);
+        $request->setClientId(static::$CLIENT_ID);
+        $request->setRedirectionUri(static::$REDIRECTION_URI_INVALID);
+        $request->setResponseType(AuthorizationResponseType::CODE);
+        $request->setState(static::$STATE);
+        
+        return $asUri ? $request->toUri() : $request;
+    }
+    
+    /**
+     * 
+     * @param boolean $asUri
+     * @return string | AuthorizationRequest
+     */
+    public function createAuthorizationRequestWithInvalidScope($asUri = false) {
+        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+        $request->addScope(static::$SCOPE_INVALID);
+        $request->setClientId(static::$CLIENT_ID);
+        $request->setRedirectionUri(static::$REDIRECTION_URI);
+        $request->setResponseType(AuthorizationResponseType::CODE);
+        $request->setState(static::$STATE);
+        
+        return $asUri ? $request->toUri() : $request;
+    }
+    
+    /**
+     * 
+     * @param boolean $asUri
+     * @return string | AuthorizationRequest
+     */
+    public function createAuthorizationRequestWithMissingRedirectionUri($asUri = false) {
+        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+        $request->addScope(AuthorizationCodeScope::BASIC);
+        $request->setClientId(static::$CLIENT_ID);
+        $request->setResponseType(AuthorizationResponseType::CODE);
+        $request->setState(static::$STATE);
+        
+        return $asUri ? $request->toUri() : $request;
+    }
+    
+    /**
+     * 
+     * @param boolean $asUri
+     * @return string | AuthorizationRequest
+     */
+    public function createAuthorizationRequestWithMissingRequiredParameter($asUri = false) {
+        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+        $request->addScope(AuthorizationCodeScope::BASIC);
+        $request->setRedirectionUri(static::$REDIRECTION_URI);
+        $request->setResponseType(AuthorizationResponseType::CODE);
+        $request->setState(static::$STATE);
+        
+        return $asUri ? $request->toUri() : $request;
+    }
+    
+    /**
+     * 
+     * @param boolean $asUri
+     * @return string | AuthorizationRequest
+     */
+    public function createAuthorizationRequestWithUnsupportedResponseType($asUri = false) {
+        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+        $request->addScope(AuthorizationCodeScope::BASIC);
+        $request->setClientId(static::$CLIENT_ID);
+        $request->setRedirectionUri(static::$REDIRECTION_URI);
+        $request->setResponseType(static::$AUTHORIZATION_RESPONSE_TYPE_UNSUPPORTED);
+        $request->setState(static::$STATE);
+        
+        return $asUri ? $request->toUri() : $request;
+    }
+}
--- src/Ms/OauthBundle/Controller/ClientController.php
+++ src/Ms/OauthBundle/Controller/ClientController.php
@@ -6,10 +6,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ms\OauthBundle\Component\Authorization\AuthorizationRequest;
 use Ms\OauthBundle\Entity\AuthorizationCodeScope;
 use Ms\OauthBundle\Component\Authorization\AuthorizationResponseType;
+use Ms\OauthBundle\Component\Authorization\AuthorizationErrorResponse;
+use Ms\OauthBundle\Component\Authorization\AccessTokenErrorResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Ms\OauthBundle\Component\Authorization\AccessTokenRequest;
 use Buzz\Message\MessageInterface;
+use Ms\OauthBundle\Component\Demo\Demo1\RequestGenerator;
 
 /**
  * Description of ClientController
@@ -22,35 +25,72 @@ class ClientController extends Controller {
      *
      * @var string
      */
+    private static $AUTHORIZATION_CODE = 'some+authorization_code';
     private static $CLIENT_ID= 'zMuobKhbnvJUTYc+EnXfRwiiHP4/OpmM5CLrdpkIsm4';
     private static $REDIRECTION_URI = 'http://msoauthphp.local/app_dev.php/client-app/demo1';
     private static $PASSWORD = 'fmodKwVrRQOC2Io7TpWu0VDkTrA=';
     private static $STATE = 'RdoTKJnaUxdRfE7QBTZX';
     /**@#-*/
+    
+    /**
+     *
+     * @var type 
+     */
+    private $requestGenerator;
+    
+    /**
+     * 
+     */
+    function __construct() {
+       $this->requestGenerator = new RequestGenerator();
+    }
 
     /**
      * 
      * @return Response
      */
     public function demo1Action(Request $request) {
+        if ($this->isAuthorizationErrorResponse($request)) {
+            return $this->displayAuthorizationError($request);
+        }
+        
+//        if ($this->isAccessTokenErrorResponse($request)) {
+//            $this->displayAccessTokenErrorResponse($request);
+//        }
+        
         if ($this->isAuthorizationCodeResponse($request)) {
             return $this->exchangeCodeForToken($request->query->get('code'));
         }
+        
 //        if ($this->isAccessTokenResponse($request)) {
 //            return new Response('Access token granted!');
 //        }
         
-        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
-        $request->addScope(AuthorizationCodeScope::BASIC);
-        $request->setClientId(static::$CLIENT_ID);
-        $request->setRedirectionUri(static::$REDIRECTION_URI);
-        $request->setResponseType(AuthorizationResponseType::CODE);
-        $request->setState(static::$STATE);
+//        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+//        $request->addScope(AuthorizationCodeScope::BASIC);
+//        $request->setClientId(static::$CLIENT_ID);
+//        $request->setRedirectionUri(static::$REDIRECTION_URI);
+//        $request->setResponseType(AuthorizationResponseType::CODE);
+//        $request->setState(static::$STATE);
         
-        return $this->render(
-            'MsOauthBundle:Client:demo_1.html.twig', 
-            array('url' => $request->toUri())
+        return $this->buildTemplate();
+    }
+    
+    /**
+     * 
+     * @return void
+     */
+    public function demo1AccessTokenMissingRequiredParameterAction() {
+        $request = $this->requestGenerator->createAccessTokenRequestWithMissingRequiredParameter(
+            static::$AUTHORIZATION_CODE
         );
+        
+        $response = $this->submitAccessTokenRequest($request);
+        if ($this->isAccessTokenErrorResponse($response)) {
+            return $this->displayAccessTokenErrorResponse($response);
+        }
+        
+        return $this->buildTemplate();
     }
     
     /**
@@ -58,14 +98,90 @@ class ClientController extends Controller {
      * @param string $code
      * @return AccessTokenRequest
      */
-    private function createAccessTokenRequest($code) {
-        $tokenRequest = new AccessTokenRequest(AccessTokenRequest::SERVER_URI);
-        $tokenRequest->setClientId(static::$CLIENT_ID);
-        $tokenRequest->setCode($code);
-        $tokenRequest->setGrantType('authorization_code');
-        $tokenRequest->setRedirectionUri(static::$REDIRECTION_URI);
+//    private function createAccessTokenRequest($code) {
+//        $tokenRequest = new AccessTokenRequest(AccessTokenRequest::SERVER_URI);
+//        $tokenRequest->setClientId(static::$CLIENT_ID);
+//        $tokenRequest->setCode($code);
+//        $tokenRequest->setGrantType('authorization_code');
+//        $tokenRequest->setRedirectionUri(static::$REDIRECTION_URI);
+//        
+//        return $tokenRequest;
+//    }
+    
+    /**
+     * 
+     * @return string
+     */
+//    private function createUrlForFullAccessTokenScenario() {
+//        $request = new AuthorizationRequest(AuthorizationRequest::SERVER_URI);
+//        $request->addScope(AuthorizationCodeScope::BASIC);
+//        $request->setClientId(static::$CLIENT_ID);
+//        $request->setRedirectionUri(static::$REDIRECTION_URI);
+//        $request->setResponseType(AuthorizationResponseType::CODE);
+//        $request->setState(static::$STATE);
+//        
+//        return $request->toUri();
+//    }
+    
+    /**
+     * 
+     * @param array $errors
+     * @return void
+     */
+    private function buildTemplate(array $errors = array()) {
+        return $this->render(
+            'MsOauthBundle:Client:demo_1.html.twig', 
+//            array('url' => $request->toUri())
+            array(
+                'errors' => $errors,
+                'url_access_token_full' => $this->requestGenerator->createAuthorizationRequest(true),
+                'url_access_token_missing_required_parameter' => 
+                    $this->generateUrl('ms_oauth_client_demo1_token_request_mrp'),
+                'url_authorization_code_invalid_client_id' =>
+                    $this->requestGenerator->createAuthorizationRequestWithInvalidClientId(true),
+                'url_authorization_code_invalid_redirection_uri' =>
+                    $this->requestGenerator->createAuthorizationRequestWithInvalidRedirectionUri(true),
+                'url_authorization_code_invalid_scope' =>
+                    $this->requestGenerator->createAuthorizationRequestWithInvalidScope(true),
+                'url_authorization_code_missing_redirection_uri' =>
+                    $this->requestGenerator->createAuthorizationRequestWithMissingRedirectionUri(true),
+                'url_authorization_code_missing_required_parameter' =>
+                    $this->requestGenerator->createAuthorizationRequestWithMissingRequiredParameter(true),
+                'url_authorization_code_unsupported_response_type' =>
+                    $this->requestGenerator->createAuthorizationRequestWithUnsupportedResponseType(true)
+            )
+        );
+    }
+    
+    /**
+     * 
+     * @param MessageInterface $response
+     * @return Response
+     */
+    private function displayAccessTokenErrorResponse(MessageInterface $response) {
+        $jsonContent = $response->getContent();
+        $content = json_decode($jsonContent);
         
-        return $tokenRequest;
+        $error = $content[AccessTokenErrorResponse::ERROR];
+        $error .= isset($content[AccessTokenErrorResponse::ERROR_DESCRIPTION])
+            ? ': ' . $content[AccessTokenErrorResponse::ERROR_DESCRIPTION]
+            : '';
+        
+        return $this->buildTemplate(array($error));
+    }
+    
+    /**
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    private function displayAuthorizationError(Request $request) {
+        $error = $request->query->get(AuthorizationErrorResponse::ERROR);
+        $error .= $request->query->has(AuthorizationErrorResponse::ERROR_DESCRIPTION)
+            ? ': ' . $request->query->get(AuthorizationErrorResponse::ERROR_DESCRIPTION)
+            : '';
+        
+        return $this->buildTemplate(array($error));
     }
     
     /**
@@ -116,11 +232,32 @@ class ClientController extends Controller {
     
     /**
      * 
+     * @param MessageInterface $response
+     * @return boolean
+     */
+    private function isAccessTokenErrorResponse(MessageInterface $response) {
+        $jsonContent = $response->getContent();
+        $content = json_decode($jsonContent);
+        
+        return isset($content[AccessTokenErrorResponse::ERROR]);
+    }
+    
+    /**
+     * 
      * @param Request $request
      * @return boolean
      */
     private function isAuthorizationCodeResponse(Request $request) {
         return $request->query->has('code');
+    }
+    
+    /**
+     * 
+     * @param Request $request
+     * @return boolean
+     */
+    private function isAuthorizationErrorResponse(Request $request) {
+        return $request->query->has(AuthorizationErrorResponse::ERROR);
     }
     
     /**
@@ -138,17 +275,17 @@ class ClientController extends Controller {
      * @return MessageInterface
      */
     private function sendAccessTokenRequest($code) {
-        $tokenRequest = $this->createAccessTokenRequest($code);
+        $request = $this->requestGenerator->createAccessTokenRequest($code);
         /* @var $buzz \Buzz\Browser */
         $buzz = $this->container->get('buzz');
         $response = $buzz->submit(
             AccessTokenRequest::SERVER_URI, 
-            $tokenRequest->toArray(),
+            $request->toArray(),
             'POST'
         );
         
         if ($this->isUnauthorizedResponse($response)) {
-            return $this->sendAccessTokenRequestWithCredentials($tokenRequest);
+            return $this->sendAccessTokenRequestWithCredentials($request);
         }
         
         return $response;
@@ -172,5 +309,21 @@ class ClientController extends Controller {
         );
         
         return $response;
+    }
+    
+    /**
+     * 
+     * @param AccessTokenRequest $request
+     * @return MessageInterface
+     */
+    private function submitAccessTokenRequest(AccessTokenRequest $request) {
+        /* @var $buzz \Buzz\Browser */
+        $buzz = $this->container->get('buzz');
+        
+        return $buzz->submit(
+            AccessTokenRequest::SERVER_URI,
+            $request->toArray(),
+            'POST'
+        );
     }
 }
