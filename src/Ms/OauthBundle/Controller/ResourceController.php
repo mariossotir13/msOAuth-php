9ae513c47ea704d36922dba01fedf0d509e0ddb6
Marios Sotiropoulos <mai1211@uom.edu.gr>
2014-02-03 11:55:01 +0200

    Add method "loadFile" to class ResourceController.
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

--- src/Ms/OauthBundle/Controller/ResourceController.php
+++ src/Ms/OauthBundle/Controller/ResourceController.php
@@ -4,6 +4,7 @@ namespace Ms\OauthBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Ms\OauthBundle\Entity\Resource;
 
 /**
  * Description of ResourceController
@@ -13,23 +14,76 @@ use Symfony\Component\HttpFoundation\Response;
 class ResourceController extends Controller {
     
     /**
+     *
+     * @var string
+     */
+    protected static $WEB_ROOT = './bundles/msdemo/';
+    
+    /**
+     * 
+     * @param string $name
+     * @return void
+     */
+    public function imageAction($name) {
+        $resource = $this->findResource($name);
+        if (empty($resource)) {
+            return $this->createNotFoundException('Could not find image: ' . $name);
+        }
+        
+        return $this->loadFile($resource);
+    }
+
+    /**
      * 
      * @param string $name
      * @return Response
      */
     public function resourceAction($name) {
-        /* @var $repository \Doctrine\Common\Persistence\ObjectRepository */
-        $repository = $this->getDoctrine()->getRepository('Ms\OauthBundle\Entity\Resource');
-        /* @var $resource \Ms\OauthBundle\Entity\Resource */
-        $resource = $repository->findOneByTitle($name);
+        $resource = $this->findResource($name);
         if(empty($resource)) {
-            return new Response('Could not find.');
+            return $this->createNotFoundException('Could not find resource: ' . $name);
         }
         
         $response = new Response($resource->getContent());
         $response->headers->set('Content-Type', $resource->getMimeType());
         
         return $response;
+    }
+
+    /**
+     * 
+     * @param string $name
+     * @return Resource
+     */
+    protected function findResource($name) {
+        /* @var $repository \Doctrine\Common\Persistence\ObjectRepository */
+        $repository = $this->getDoctrine()->getRepository('Ms\OauthBundle\Entity\Resource');
+        
+        return $repository->findOneByTitle($name);
+    }
+    
+    /**
+     * Η υλοποίηση βασίζεται στο εγχειρίδιο χρήσης της συνάρτησης [readfile][1].
+     * 
+     * 
+     * [1]: http://www.php.net/readfile
+     *      "PHP: readfile - Manual"
+     * 
+     * @param Resource $resource
+     * @return void
+     */
+    protected function loadFile(Resource $resource) {
+        header('Content-Type: ' . $resource->getMimeType());
+        ob_clean();
+        flush();
+        
+        $path = realpath(static::$WEB_ROOT . $resource->getContent());
+        $fileInfo = new \SplFileInfo($path);
+        if ($fileInfo->isFile() && $fileInfo->isReadable()) {
+            readfile($fileInfo->getRealPath());
+        }
+        
+        exit;
     }
 }
 
--- src/Ms/OauthBundle/DataFixtures/ORM/LoadResourceData.php
+++ src/Ms/OauthBundle/DataFixtures/ORM/LoadResourceData.php
@@ -14,21 +14,47 @@ use Ms\OauthBundle\Entity\Resource;
  */
 class LoadResourceData extends AbstractFixture implements OrderedFixtureInterface{
     
+    /**
+     * @inheritdoc
+     */
     public function getOrder() {
         return 5;
     }
 
+    /**
+     * @inheritdoc
+     */
     public function load(ObjectManager $manager) {
+        $manager->persist($this->createImageResource());
+        $manager->persist($this->createTextResource());
         
+        $manager->flush();
+    }
+    
+    /**
+     * 
+     * @return Resource
+     */
+    private function createImageResource() {
         $resource = new Resource();
+        $resource->setContent('images/img_1.jpg')
+                ->setMimeType('image/jpg')
+                ->setTitle('1');
         
+        return $resource;
+    }
+    
+    /**
+     * 
+     * @return Resource
+     */
+    private function createTextResource() {
+        $resource = new Resource();
         $resource->setContent('Lorem ipsum dolor sit amet.')
                 ->setMimeType('text/plain')
                 ->setTitle('quote');
         
-        $manager->persist($resource);
-        
-        $manager->flush();
-    }    
+        return $resource;
+    }
 }
 
