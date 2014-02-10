4f054aacf8ca0d031d40331dabcc6ae256960ee5
Marios Sotiropoulos <mai1211@uom.edu.gr>
2014-02-10 16:23:53 +0200

    Load ResourceGroup data.
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

--- src/Ms/OauthBundle/DataFixtures/ORM/LoadResourceData.php
+++ src/Ms/OauthBundle/DataFixtures/ORM/LoadResourceData.php
@@ -12,8 +12,58 @@ use Ms\OauthBundle\Entity\Resource;
  *
  * @author Marios
  */
-class LoadResourceData extends AbstractFixture implements OrderedFixtureInterface{
+class LoadResourceData extends AbstractFixture implements OrderedFixtureInterface {
     
+    /**#@+
+     * 
+     * @var string
+     */
+    const REF_RESOURCE_IMG_VANGOGH_1 = 'ref_resource_img_vangogh_1';
+    const REF_RESOURCE_IMG_VANGOGH_2 = 'ref_resource_img_vangogh_2';
+    const REF_RESOURCE_IMG_VANGOGH_3 = 'ref_resource_img_vangogh_3';
+    const REF_RESOURCE_IMG_VANGOGH_4 = 'ref_resource_img_vangogh_4';
+    const REF_RESOURCE_IMG_VANGOGH_5 = 'ref_resource_img_vangogh_5';
+    /**#@-*/
+
+    /**
+     * Κάθε στοιχείο έχει την εξής δομή:
+     * 
+     *      array(
+     *          'path' => {path to the image file},
+     *          'reference' => {name of the entity reference},
+     *          'title' => {image title}
+     *      )
+     * 
+     * @var array
+     */
+    private static $IMAGES = array(
+        array(
+            'path' => 'images/a-cafe-night-on-place-lamartine-in-arles.jpg',
+            'reference' => self::REF_RESOURCE_IMG_VANGOGH_1,
+            'title' => 'A Cafe Night At Lamartine In Arles'
+        ),
+        array(
+            'path' => 'images/a-pair-of-shoes.jpg',
+            'reference' => self::REF_RESOURCE_IMG_VANGOGH_2,
+            'title' => 'A Pair of Shoes'
+        ),
+        array(
+            'path' => 'images/first-steps.jpg',
+            'reference' => self::REF_RESOURCE_IMG_VANGOGH_3,
+            'title' => 'First Steps'
+        ),
+        array(
+            'path' => 'images/langlois-bridge-at-arles-with-women-washing.jpg',
+            'reference' => self::REF_RESOURCE_IMG_VANGOGH_4,
+            'title' => 'Langlois Bridge At Arles With Women Washing'
+        ),
+        array(
+            'path' => 'images/starry-night.jpg',
+            'reference' => self::REF_RESOURCE_IMG_VANGOGH_5,
+            'title' => 'Starry Night'
+        )
+    );
+
     /**
      * @inheritdoc
      */
@@ -25,36 +75,28 @@ class LoadResourceData extends AbstractFixture implements OrderedFixtureInterfac
      * @inheritdoc
      */
     public function load(ObjectManager $manager) {
-        $manager->persist($this->createImageResource());
-        $manager->persist($this->createTextResource());
-        
-        $manager->flush();
+        foreach (static::$IMAGES as $image) {
+            $resource = $this->createImageResource($image['path'], $image['title']);
+            $manager->persist($resource);
+            $manager->flush();
+            
+            $this->addReference($image['reference'], $resource);
+        }
     }
-    
-    /**
-     * 
-     * @return Resource
-     */
-    private function createImageResource() {
-        $resource = new Resource();
-        $resource->setContent('images/img_1.jpg')
-                ->setMimeType('image/jpg')
-                ->setTitle('1');
-        
-        return $resource;
-    }
-    
-    /**
-     * 
-     * @return Resource
-     */
-    private function createTextResource() {
-        $resource = new Resource();
-        $resource->setContent('Lorem ipsum dolor sit amet.')
-                ->setMimeType('text/plain')
-                ->setTitle('quote');
-        
-        return $resource;
-    }
-}
 
+    /**
+     * 
+     * @param string $path
+     * @param string $title
+     * @return Resource
+     */
+    private function createImageResource($path, $title) {
+        $resource = new Resource();
+        $resource->setContent($path)
+            ->setMimeType('image/jpg')
+            ->setTitle($title);
+
+        return $resource;
+    }
+
+}
---
+++ src/Ms/OauthBundle/DataFixtures/ORM/LoadResourceGroupData.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ms\OauthBundle\DataFixtures\ORM;
+
+use Ms\OauthBundle\Entity\ResourceGroup;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Ms\OauthBundle\DataFixtures\ORM\LoadResourceData;
+
+/**
+ * Description of LoadResourceGroupData
+ *
+ * @author Marios
+ */
+class LoadResourceGroupData extends AbstractFixture implements OrderedFixtureInterface {
+    
+    /**
+     * @inheritdoc
+     */
+    public function getOrder() {
+        return 6;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function load(ObjectManager $manager) {
+        $group = new ResourceGroup();
+        $group->setTitle('Van Gogh Paintings')
+            ->addResource($this->getReference(LoadResourceData::REF_RESOURCE_IMG_VANGOGH_1))
+            ->addResource($this->getReference(LoadResourceData::REF_RESOURCE_IMG_VANGOGH_2))
+            ->addResource($this->getReference(LoadResourceData::REF_RESOURCE_IMG_VANGOGH_3))
+            ->addResource($this->getReference(LoadResourceData::REF_RESOURCE_IMG_VANGOGH_4))
+            ->addResource($this->getReference(LoadResourceData::REF_RESOURCE_IMG_VANGOGH_5));
+        $manager->persist($group);
+        
+        $manager->flush();
+    }
+}
