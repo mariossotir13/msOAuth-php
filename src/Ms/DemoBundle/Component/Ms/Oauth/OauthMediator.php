c149a45fd5238f266c5a661de3b4453d6b88e3f2
Marios Sotiropoulos <mai1211@uom.edu.gr>
2014-02-12 03:59:59 +0200

    Enable the OauthMediator to send requests for "authorization codes" and "access tokens".
    Signed-off-by: Marios Sotiropoulos <mai1211@uom.edu.gr>

--- src/Ms/DemoBundle/Component/Ms/Oauth/OauthMediator.php
+++ src/Ms/DemoBundle/Component/Ms/Oauth/OauthMediator.php
@@ -5,8 +5,14 @@ namespace Ms\DemoBundle\Component\Ms\Oauth;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ms\DemoBundle\Component\Ms\Oauth\RequestGenerator;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Buzz\Browser;
 use Buzz\Message\Response as BuzzResponse;
+use Ms\OauthBundle\Component\Authorization\AuthorizationErrorResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Ms\OauthBundle\Component\Authorization\AccessTokenRequest;
+use Ms\OauthBundle\Component\Authorization\AccessTokenResponse;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Description of OauthMediator
@@ -17,6 +23,20 @@ class OauthMediator {
     
     /**
      *
+     * @var string
+     */
+    private static $ACCESS_TOKEN_FILE_INDEX_TOKEN = 'token';
+
+    /**#@+
+     * 
+     * @var string
+     */
+    private static $CLIENT_ID= 'zMuobKhbnvJUTYc+EnXfRwiiHP4/OpmM5CLrdpkIsm4';
+    private static $PASSWORD = 'fmodKwVrRQOC2Io7TpWu0VDkTrA=';
+    /**#@-*/
+    
+    /**
+     *
      * @var string[]
      */
     private static $RESPONSE_HEADERS = array(
@@ -24,6 +44,18 @@ class OauthMediator {
         'Content-Type',
         'WWW-Authenticate'
     );
+    
+    /**
+     *
+     * @var string
+     */
+    private static $SESSION_KEY_REFERER = 'ms_demo_referer';
+    
+    /**
+     *
+     * @var string
+     */
+    private $accessToken;
     
     /**
      *
@@ -44,6 +76,95 @@ class OauthMediator {
     function __construct(Controller $controller) {
         $this->controller = $controller;
         $this->requestGenerator = new RequestGenerator();
+    }
+    
+    /**
+     * 
+     * @param string $code
+     * @return Response
+     */
+    public function exchangeAuthorizationCodeForAccessToken($code) {
+//        $response = $this->sendAccessTokenRequest($code);        
+//        $content = json_decode($response->getContent(), true);
+//        if (isset($content['access_token'])) {
+//            return new Response('Access Token: ' . $content['access_token']);
+//        }
+//        
+//        return new Response('Error: ' . $content['error'] . '<br />'
+//            . 'Error Description: ' . $content['error_description']);
+        
+        $response = $this->sendAccessTokenRequest($code);
+        if (!$this->isAccessTokenErrorResponse($response)) {
+            $content = json_decode($response->getContent(), true);
+            $this->setAccessToken($content['access_token']);
+        }
+        
+        return $response;
+    }
+    
+    /**
+     * 
+     * @return string
+     */
+    public function getAccessToken() {
+        if (!empty($this->accessToken)) {
+            return $this->accessToken;
+        }
+        
+        /* @var $container \Symfony\Component\DependencyInjection\Container */
+        $container = $this->controller->get('service_container');
+        $filePath = $container->getParameter('access_token_file');
+        $fileInfo = new \SplFileInfo($filePath);
+        if (!$fileInfo->isFile()) {
+            return '';
+        }
+        
+        $file = $fileInfo->openFile();
+        $contents = json_decode($file->fgets(), true);
+        
+        $this->setAccessToken($contents[static::$ACCESS_TOKEN_FILE_INDEX_TOKEN]);
+        
+        return $this->accessToken;
+    }
+    
+    /**
+     * 
+     * @param Request $request
+     * @return string
+     */
+    public function getAuthorizationCodeFromResponse(Request $request) {
+        return $this->isAuthorizationCodeResponse($request)
+            ? $request->query->get('code')
+            : '';
+    }
+    
+    /**
+     * 
+     * @param JsonResponse $response
+     * @return return bool
+     */
+    public function isAccessTokenErrorResponse(JsonResponse $response) {
+        $content = json_decode($response->getContent(), true);
+        
+        return empty($content['access_token']);
+    }
+    
+    /**
+     * 
+     * @param Request $request
+     * @return boolean
+     */
+    public function isAuthorizationCodeErrorResponse(Request $request) {
+        return $request->query->has(AuthorizationErrorResponse::ERROR);
+    }
+    
+    /**
+     * 
+     * @param Request $request
+     * @return bool
+     */
+    public function isAuthorizationCodeResponse(Request $request) {
+        return $request->query->has('code');
     }
     
     /**
@@ -79,25 +200,20 @@ class OauthMediator {
      * 
      * @return Response
      */
-    public function requestAccessToken() {
-        $response = $this->requestAuthorizationCode();
+    public function requestAuthorizationCode() {
+        $this->storeReferer();
+        $authorizationCodeRequestUrl = $this->requestGenerator->createAuthorizationRequest(true);
         
-        return $response;
+        return $this->controller->redirect($authorizationCodeRequestUrl);
     }
     
     /**
      * 
      * @return Response
      */
-    public function requestAuthorizationCode() {
-        $authorizationCodeRequestUrl = $this->requestGenerator->createAuthorizationRequest(true);
-//        $browser = $this->getBrowser();
-//        $response = $browser->get($authorizationCodeRequestUrl);
-//        
-//        return $this->transformBuzzToSymfonyResponse($response);
-        
-        return $this->controller->redirect($authorizationCodeRequestUrl);
-    }
+//    public function requestAccessToken() {
+//        return $this->requestAuthorizationCode();
+//    }
     
     /**
      * 
@@ -118,10 +234,95 @@ class OauthMediator {
     
     /**
      * 
-     * @param BuzzResponse $response
-     * @return Response
+     * @return string
      */
-    protected function transformBuzzToSymfonyResponse(BuzzResponse $response) {
+    public function getReferer() {
+        /* @var $session Session */
+        $session = $this->controller->get('session');
+        
+        return $session->get(static::$SESSION_KEY_REFERER);
+    }
+    
+    /**
+     * 
+     * @param string $authorizationCode
+     * @return JsonResponse
+     */
+    protected function sendAccessTokenRequest($authorizationCode) {
+        $request = $this->requestGenerator->createAccessTokenRequest($authorizationCode);
+        $browser = $this->getBrowser();
+        $buzzResponse = $browser->submit(
+            AccessTokenRequest::SERVER_URI, 
+            $request->toArray(),
+            'POST'
+        );
+
+        $response = $this->transformBuzzToSymfonyResponse($buzzResponse, true);
+        if ($this->isUnauthenticatedResponse($response)) {
+            return $this->sendAccessTokenRequestWithCredentials($request);
+        }
+        
+        return $response;
+    }
+    
+    /**
+     * 
+     * @return JsonResponse
+     */
+    protected function sendAccessTokenRequestWithCredentials(AccessTokenRequest $tokenRequest) {
+        $credentials = base64_encode(static::$CLIENT_ID . ':' . static::$PASSWORD);
+        $browser = $this->getBrowser();
+        $buzzResponse = $browser->submit(
+            AccessTokenRequest::SERVER_URI,
+            $tokenRequest->toArray(),
+            'POST',
+            array('Authorization' => 'Basic ' . $credentials)
+        );
+                
+        return $this->transformBuzzToSymfonyResponse($buzzResponse, true);
+    }
+    
+    /**
+     * 
+     * @param string $token
+     * @return void
+     */
+    protected function setAccessToken($token) {
+        /* @var $container \Symfony\Component\DependencyInjection\Container */
+        $container = $this->controller->get('service_container');
+        $filePath = $container->getParameter('access_token_file');
+        $fileInfo = new \SplFileInfo($filePath);
+        if (!$fileInfo->isFile()) {
+            $basePath = $fileInfo->getPath();
+            mkdir($basePath, 0777, true);
+        }
+        
+        $file = $fileInfo->openFile('w');
+        $contents = json_encode(array(static::$ACCESS_TOKEN_FILE_INDEX_TOKEN => $token));
+        $file->fwrite($contents);
+        
+        $this->accessToken = $token;
+    }
+    
+    /**
+     * 
+     * @return void
+     */
+    protected function storeReferer() {
+        /* @var $request Request */
+        $request = $this->controller->get('request');
+        /* @var $session Session */
+        $session = $this->controller->get('session');
+        $session->set(static::$SESSION_KEY_REFERER, $request->getRequestUri());
+    }
+    
+    /**
+     * 
+     * @param BuzzResponse $response
+     * @param bool $json
+     * @return Response|JsonResponse
+     */
+    protected function transformBuzzToSymfonyResponse(BuzzResponse $response, $json = false) {
         $headers = array();
         foreach (static::$RESPONSE_HEADERS as $name) {
             if ($response->getHeader($name)) {
@@ -129,6 +330,29 @@ class OauthMediator {
             }
         }
         
-        return new Response($response->getContent(), $response->getStatusCode(), $headers);
+        $content = is_string($response->getContent()) ? $response->getContent() : '';
+        if ($json) {
+            return new JsonResponse(json_decode($content, true), $response->getStatusCode(), $headers);
+        }
+        
+        return new Response($content, $response->getStatusCode(), $headers);
     }
+    
+    /**
+     * 
+     * @param Response $response
+     * @return JsonResponse
+     */
+//    protected function transformBuzzToSymfonyJsonResponse(BuzzResponse $response) {
+//        $headers = array();
+//        foreach (static::$RESPONSE_HEADERS as $name) {
+//            if ($response->getHeader($name)) {
+//                $headers[$name] = $response->getHeader($name);
+//            }
+//        }
+//        
+//        $content = is_array($response->getContent()) ? $response->getContent() : array();
+//        
+//        return new JsonResponse($content, $response->getStatusCode(), $headers);
+//    }
 }
